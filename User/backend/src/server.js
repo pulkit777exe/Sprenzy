4e25d0f368ee6909a556430c343daf24f20f4faa
pulkit777exe <pulkitpareek88@gmail.com>
2025-01-30 22:43:41 +0530

    try-5 of fixing cors

--- User/backend/src/server.js
+++ User/backend/src/server.js
@@ -8,6 +8,7 @@ import { productRouter } from './routes/Product.routes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.VITE_APP_URL;
+const BACKEND_URL = process.env.VITE_BACKEND_URL;
 
 app.use(express.json());
 const whitelist = [FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174']
@@ -28,8 +29,8 @@ app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/product", productRouter);
+app.use(`${BACKEND_URL}/api/v1/user`, userRouter);
+app.use(`${BACKEND_URL}/api/v1/product`, productRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
