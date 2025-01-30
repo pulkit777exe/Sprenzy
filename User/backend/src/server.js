25f1a26f02a8fb91fda703b16752ee1b8abf4a5f
pulkit777exe <pulkitpareek88@gmail.com>
2025-01-30 23:09:42 +0530

    try-8 of fixing cors

--- User/backend/src/server.js
+++ User/backend/src/server.js
@@ -8,29 +8,21 @@ import { productRouter } from './routes/Product.routes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.VITE_APP_URL;
-const BACKEND_URL = process.env.VITE_BACKEND_URL;
+
+
+app.use(cors({
+  origin: 'https://royal-choice.netlify.app/',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+}))
 
 app.use(express.json());
-const whitelist = [FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true
-}
-
-app.use(cors(corsOptions)); 
-
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
-app.use(`${BACKEND_URL}/api/v1/user`, userRouter);
-app.use(`${BACKEND_URL}/api/v1/product`, productRouter);
+app.use(`/api/v1/user`, userRouter);
+app.use(`/api/v1/product`, productRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
