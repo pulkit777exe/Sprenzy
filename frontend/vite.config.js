24b638c237a1ef498f4771e761a081d9246cb3c9
pulkit777exe <pulkitpareek88@gmail.com>
2025-03-17 19:11:11 +0530

    Revert "Try-2 of fixing 404 error"
    This reverts commit 5330fa5e8ff28e14be3dce17faeb3d416c88de46.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.VITE_BACKEND_API_URL': JSON.stringify(env.VITE_BACKEND_API_URL),
-      'process.env.VITE_GOOGLE_AUTH_CLIENT_ID': JSON.stringify(env.VITE_GOOGLE_AUTH_CLIENT_ID),
-      'process.env.VITE_PAYONEER_MERCHANT_ID': JSON.stringify(env.VITE_PAYONEER_MERCHANT_ID)
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
     },
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: env.VITE_BACKEND_API_URL,
-          changeOrigin: true,
-          secure: false,
-        }
-      }
-    }
-  }
+  },
+  plugins: [react()],
 })
